a37d65dc1629fb02e1f642f6e4f38ffa348e37f2
Doug Gale <doug16k@gmail.com>
2021-05-30 08:55:08 +0000

    Remove stupid image and listen on all interfaces

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 #!/bin/env node
 var express = require('express'),
     app = express(),
-    ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    ipaddress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 app.get('/dumpenv', function(req, res) {
